d1f2cfbb4698a21cd45fcffd55eacf82bae77932
Praveen Adithya B <157034812+pravee-n2005@users.noreply.github.com>
2025-04-14 12:17:41 +0530

    Add files via upload

---
+++ input,output,typecasting.py
@@ -0,0 +1,8 @@
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+sum = num1 + num2
+print("Sum of two numbers is: ", sum)
+product = num1 * num2
+print("Product of two numbers is: ", product)
+average = (num1 + num2) / 2
+print("Average of two numbers is: ", average)
---
+++ variables,datatypes.py
@@ -0,0 +1,5 @@
+name = "Praveen"
+age = 19
+height = 5.7  
+is_student = True
+print("Hi, my name is", "Praveen"".", "I am", 19, "years old,", 5.7, "feet tall, and I am a student.")
